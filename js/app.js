426b79fe7c716440e71158dbf699505455abf641
sdenson <shaniyad97@gmail.com>
2022-09-12 13:14:38 -0500

    finish cardPicked functions

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ let deck1 = []
 let deck2 = []
 let deck3 = []
 let deck4 = []
-let cardToRemove
+let cardToRemove1
+let cardToRemove2
 let winner
 /*-------------------------------- Constants ---------------------------*/
 let fullDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
@@ -16,6 +17,10 @@ let fullDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d
 
 const messageEl = document.querySelector('#message')
 const resetBtnEl = document.querySelector('#resetBtn')
+let deck1El = document.getElementById('deck-1')
+let deck2El = document.getElementById('deck-2')
+let deck3El = document.getElementById('deck-3')
+let deck4El = document.getElementById('deck-4')
 
 
 /*----------------------------- Event Listeners -------------------------*/
@@ -78,45 +83,84 @@ function war() {
 
 function handleClick() {
   if (fullDeck.length > 0) {
-    // Randomly select number from total cards remaining
     
     // Assign card with the random index to a variable
-    let cardPicked = fullDeck.splice(shuffledDeck, 1)[0]
+    let cardPicked1 = fullDeck.splice(shuffledDeck, 1)[0]
+    let cardPicked2 = fullDeck.splice(shuffledDeck, 1)[0]
     // Add card picked to deck 2
-    deck1.push(cardPicked)
+    deck1.push(cardPicked1)
+    deck3.push(cardPicked2)
     // Pass card picked to render function to display
-    render(cardPicked)
+    render()
+    
+    
   }
 }
 
 // Function to render deck state
-function render(cardPicked) {
-  console.log(cardPicked)
-  // Remove outline class when first card is picked
-  // if (deck2.length === 1) {
-  //   deck2El.classList.remove('outline')
-  // }
-  // // Removes previous picked card from deck 2 class list
-  // if (deck2.length > 1) {
-  //   deck2El.classList.remove(cardToRemove)
-  // }
+function playerDeck(cardPicked1) {
 
-  // cardToRemove = cardPicked
+  // Remove outline class when first card is picked
+  if (deck2.length === 1) {
+    deck2El.classList.remove('outline')
+  }
+  // // Removes previous picked card from deck 2 class list
+  if (deck2.length > 1) {
+    deck2El.classList.remove(cardToRemove1)
+
+  }
+
+  cardToRemove1 = cardPicked1
   // // Add current card picked to deck 2 element
-  // deck2El.classList.add(cardPicked)
+  deck2El.classList.add(cardPicked1)
+  
+
   // // Adjust shadow when deck gets above/below halfway full
-  // if (deck2.length === 26) {
-  //   deck2El.classList.add('shadow')
-  //   deck1El.classList.remove('shadow')
-  // }
+  if (deck2.length === 26 && deck4.length === 26) {
+    deck2El.classList.add('shadow')
+    deck1El.classList.remove('shadow')
+  
+  }
   // // Remove card back color and add outline when last card is picked
   // if (deck1.length === 0) {
   //   deck1El.classList.add('outline')
   //   deck1El.classList.remove('back-blue')
+  //   deck3El.classList.add('outline')
+  //   deck3El.classList.remove('back-blue')
   // }
 
 }
 
+function computerDeck(cardPicked2) {
+if (deck4.length === 1) {
+  deck4El.classList.remove('outline')
+}
+// // Removes previous picked card from deck 2 class list
+if (deck4.length) {
+  deck4El.classList.remove(cardToRemove2)
+
+}
+cardToRemove2 = cardPicked2
+// // Add current card picked to deck 2 element
+
+deck4El.classList.add(cardPicked2)
+
+
+// // Adjust shadow when deck gets above/below halfway full
+if (deck4.length === 26) {
+
+  deck4El.classList.add('shadow')
+  deck3El.classList.remove('shadow')
+}
+// // Remove card back color and add outline when last card is picked
+// if (deck1.length === 0) {
+//   deck1El.classList.add('outline')
+//   deck1El.classList.remove('back-blue')
+//   deck3El.classList.add('outline')
+//   deck3El.classList.remove('back-blue')
+// }
+
+}
 
 
 
@@ -168,5 +212,4 @@ function render(cardPicked) {
 //     render(cardPicked)
 //   }
 // }
-
-
+// 
